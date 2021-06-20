284aff554672257774b99b8e5bf29c6c8e57ee29
Kamran Choudhry <theblizzzzard@gmail.com>
2021-06-19 23:46:21 -0400

    NEW: init

---
+++ Trade.py
@@ -0,0 +1,10 @@
+class Trade(object):
+    def __init__(self, incoming_side, incoming_price, trade_size, incoming_order_id, book_order_id):
+        self.side = incoming_side
+        self.price = incoming_price
+        self.size = trade_size
+        self.incoming_order_id = incoming_order_id
+        self.book_order_id = book_order_id
+
+    def __repr__(self):
+        return 'Executed: {0} {1} units at {2}'.format(self.side, self.size, self.price)
